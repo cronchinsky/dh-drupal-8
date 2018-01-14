2956a98763864816b97e15fd5487d7039a8ab3e7
Carolyn Ronchinsky <cronchinsky@edc.org>
2018-01-14 17:47:41 -0500

    8.2->8.4 updated files

--- vendor/commerceguys/addressing/scripts/generate.php
+++ vendor/commerceguys/addressing/scripts/generate.php
@@ -34,7 +34,7 @@ foreach ($neededDirectories as $neededDirectory) {
 $countryRepository = new CountryRepository();
 $countries = $countryRepository->getList();
 ksort($countries);
-$serviceUrl = 'http://i18napis.appspot.com/address';
+$serviceUrl = 'https://chromium-i18n.appspot.com/ssl-address';
 
 echo "Generating the url list.\n";
 
@@ -50,7 +50,7 @@ exec('cd raw && aria2c -u 16 -i url_list.txt');
 $foundCountries = ['ZZ'];
 $index = file_get_contents($serviceUrl);
 foreach ($countries as $countryCode => $countryName) {
-    $link = "<a href='/address/data/{$countryCode}'>";
+    $link = "<a href='/ssl-address/data/{$countryCode}'>";
     // This is still faster than running a file_exists() for each country code.
     if (strpos($index, $link) !== false) {
         $foundCountries[] = $countryCode;
@@ -77,11 +77,12 @@ foreach ($foundCountries as $countryCode) {
     }
     $addressFormat = create_address_format_definition($countryCode, $definition);
 
-    // Create a list of available translations.
-    // Ignore Hong Kong because the listed translation (English) is already
-    // provided through the lname property.
+    // Get the French subdivision names for Canada.
+    // This mechanism can only work for countries with a single
+    // alternative language and ISO-based subdivision codes
+    // (URL example: data/CA/AB and data/CA/AB--fr).
     $languages = [];
-    if (isset($definition['languages']) && $countryCode != 'HK') {
+    if ($countryCode == 'CA' && isset($definition['languages'])) {
         $languages = explode('~', $definition['languages']);
         array_shift($languages);
     }
@@ -98,20 +99,20 @@ foreach ($foundCountries as $countryCode) {
 
     $addressFormats[$countryCode] = $addressFormat;
 }
+
+echo "Writing the final definitions to disk.\n";
+// Subdivisions are stored in JSON.
+foreach ($groupedSubdivisions as $parentId => $subdivisions) {
+    file_put_json('subdivision/' . $parentId . '.json', $subdivisions);
+}
 // Generate the subdivision depths for each country.
 $depths = generate_subdivision_depths($foundCountries);
 foreach ($depths as $countryCode => $depth) {
     $addressFormats[$countryCode]['subdivision_depth'] = $depth;
 }
-
-echo "Writing the final definitions to disk.\n";
 // Address formats are stored in PHP, then manually transferred to
 // AddressFormatRepository.
 file_put_php('address_formats.php', $addressFormats);
-// Subdivisions are stored in JSON.
-foreach ($groupedSubdivisions as $parentId => $subdivisions) {
-    file_put_json('subdivision/' . $parentId . '.json', $subdivisions);
-}
 
 echo "Done.\n";
 
@@ -167,9 +168,9 @@ function generate_url_list()
     global $serviceUrl;
 
     $index = file_get_contents($serviceUrl);
-    // Get all links that start with /address/data.
+    // Get all links that start with /ssl-address/data.
     // This avoids the /address/examples urls which aren't needed.
-    preg_match_all("/<a\shref=\'\/address\/data\/([^\"]*)\'>/siU", $index, $matches);
+    preg_match_all("/<a\shref=\'\/ssl-address\/data\/([^\"]*)\'>/siU", $index, $matches);
     // Assemble the urls
     $list = array_map(function ($href) use ($serviceUrl) {
         // Replace the url encoded single slash with a real one.
@@ -233,7 +234,7 @@ function generate_subdivisions($countryCode, array $parents, $subdivisionPaths, 
             unset($subdivisions[$group]['locale']);
         }
         // Generate the subdivision.
-        $subdivisions[$group]['subdivisions'][$code] = create_subdivision_definition($countryCode, $definition);
+        $subdivisions[$group]['subdivisions'][$code] = create_subdivision_definition($countryCode, $code, $definition);
 
         if (isset($definition['sub_keys'])) {
             $subdivisions[$group]['subdivisions'][$code]['has_children'] = true;
@@ -378,18 +379,18 @@ function create_address_format_definition($countryCode, $rawDefinition)
 /**
  * Creates a subdivision definition from Google's raw definition.
  */
-function create_subdivision_definition($countryCode, $rawDefinition)
+function create_subdivision_definition($countryCode, $code, $rawDefinition)
 {
     $subdivision = [];
     if (isset($rawDefinition['lname'])) {
-        // The lname was already chosen for the code in the parent function,
-        // don't need to store it as the name cause SubdivisionRepository
-        // optimizes for that.
         $subdivision['local_code'] = $rawDefinition['key'];
         if (isset($rawDefinition['name']) && $rawDefinition['key'] != $rawDefinition['name']) {
             $subdivision['local_name'] = $rawDefinition['name'];
         }
-    } elseif (isset($rawDefinition['name'])) {
+        if ($code != $rawDefinition['lname']) {
+            $subdivision['name'] = $rawDefinition['lname'];
+        }
+    } elseif (isset($rawDefinition['name']) && $rawDefinition['key'] != $rawDefinition['name']) {
         $subdivision['name'] = $rawDefinition['name'];
     }
     if (isset($rawDefinition['isoid'])) {
